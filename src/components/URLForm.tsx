ef9c4b176ca3006ac49ab1d556b4cfa09ce6bfbf
Josiah Ngu <josiahngu@gmail.com>
2020-08-25 18:55:38 -0500

    initial commit of the homepage UI with submission form

---
+++ src/components/URLForm.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./URLForms.scss";
+type FormElem = React.FormEvent<HTMLFormElement>;
+type InputElem = React.FormEvent<HTMLInputElement>;
+const URLForm = (): JSX.Element => {
+  const [originalURL, setOriginalURL] = useState<string>("");
+  const [keyword, setKeyword] = useState("");
+  const submitHandler = (event: FormElem): void => {
+    event.preventDefault();
+  };
+  const keywordHandler = (event: InputElem): void => {
+    setKeyword(event.currentTarget.value.substring(0, 5));
+  };
+  // const linkHandler = (event: InputElem): void => {
+  //   const linkRegex = "";
+  // };
+  return (
+    <div id="URLFormMain">
+      <div></div>
+      <form onSubmit={submitHandler} className="Form">
+        <h1>Thiner</h1>
+        <label>URL</label>
+        <br />
+        <input
+          type="text"
+          value={originalURL}
+          required
+          onChange={(event) => {
+            setOriginalURL(event.target.value);
+          }}
+        />
+        <br />
+        <label>Magic Word (Max Chars: 5)</label>
+        <br />
+        <input value={keyword} onChange={keywordHandler} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+export default URLForm;
